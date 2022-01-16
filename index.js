f20b4c14079ae0796871bf8d2c59b9b8f34c06e7
Trevor Vardeman <trevortx@gmail.com>
2022-01-16 11:32:59 -0800

    1/2 assignments

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const names = ["Jolene", "Chris", "Princess"];
+const birthday = "birthday";
+let newArray = [];
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+}
+
+return newArray;
+}
+
+console.log(writeCards(names, birthday));
